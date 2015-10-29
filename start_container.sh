53ae9ecc80539356d54399c7369826acd41d633a
clodio <claude.seguret@gmail.com>
2015-10-29 11:13:53 +0100

    Ugrade to 2.5.0

--- start_container.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker run -i -p 3001:3000 \
-  -v /home/torkel/dev/grafana-docker/data:/var/lib/grafana \
-  -e "GF_SERVER_ROOT_URL=http://grafana.server.name"  \
-  grafana/grafana:2.0.1
