9026d2fe7ec3538564bf3ea89b5a8e986f87d672
Daniel Lee <dan.limerick@gmail.com>
2016-01-28 18:58:10 +0100

    if not master build push latest tag to docker hub too

--- push_to_docker_hub.sh
+++ push_to_docker_hub.sh
@@ -3,6 +3,8 @@ _grafana_tag=$1
 if [ -z "${_grafana_tag}" ]; then
 	source GRAFANA_VERSION
 	_grafana_tag=$GRAFANA_VERSION
+	docker push grafana/grafana:${_grafana_tag}
+	docker push grafana/grafana:latest
+else
+	docker push grafana/grafana:${_grafana_tag}
 fi
-
-docker push grafana/grafana:${_grafana_tag}
