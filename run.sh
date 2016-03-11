61ed92a9a7f2b3ce09f25cdb049f44e2be7d70c4
Jason Yan <tailofthesun@gmail.com>
2016-03-10 17:59:28 -0800

    Added support for AWS credentials.

--- run.sh
+++ run.sh
@@ -1,6 +1,29 @@
-#!/bin/sh -e
+#!/bin/bash -e
 
 chown -R grafana:grafana /var/lib/grafana /var/log/grafana
+
+if [ ! -z ${GF_AWS_PROFILES+x} ]; then
+    mkdir -p ~grafana/.aws/
+    touch ~grafana/.aws/credentials
+
+    for profile in ${GF_AWS_PROFILES}; do
+        access_key_varname="GF_AWS_${profile}_ACCESS_KEY_ID"
+        secret_key_varname="GF_AWS_${profile}_SECRET_ACCESS_KEY"
+        region_varname="GF_AWS_${profile}_REGION"
+
+        if [ ! -z "${!access_key_varname}" -a ! -z "${!secret_key_varname}" ]; then
+            echo "[${profile}]" >> ~grafana/.aws/credentials
+            echo "aws_access_key_id = ${!access_key_varname}" >> ~grafana/.aws/credentials
+            echo "aws_secret_access_key = ${!secret_key_varname}" >> ~grafana/.aws/credentials
+            if [ ! -z "${!region_varname}" ]; then
+                echo "region = ${!region_varname}" >> ~grafana/.aws/credentials
+            fi
+        fi
+    done
+
+    chown grafana:grafana -R ~grafana/.aws
+    chmod 600 ~grafana/.aws/credentials
+fi
 
 exec gosu grafana /usr/sbin/grafana-server  \
   --homepath=/usr/share/grafana             \
