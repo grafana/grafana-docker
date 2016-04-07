256b449171924510e46f68ff486ad86da252c3e7
bergquist <carl.bergquist@gmail.com>
2016-04-07 10:03:06 +0200

    adds plugin to startup

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 
 : "${GF_PATHS_DATA:=/var/lib/grafana}"
 : "${GF_PATHS_LOGS:=/var/log/grafana}"
+: "${GF_PATHS_PLUGINS:=/var/lib/grafana/plugins}"
 
 chown -R grafana:grafana "$GF_PATHS_DATA" "$GF_PATHS_LOGS"
 chown -R grafana:grafana /etc/grafana
@@ -10,4 +11,5 @@ exec gosu grafana /usr/sbin/grafana-server  \
   --homepath=/usr/share/grafana             \
   --config=/etc/grafana/grafana.ini         \
   cfg:default.paths.data="$GF_PATHS_DATA"   \
-  cfg:default.paths.logs="$GF_PATHS_LOGS"
+  cfg:default.paths.logs="$GF_PATHS_LOGS"   \
+  cfg:default.paths.plugins="$GF_PATHS_PLUGINS"
