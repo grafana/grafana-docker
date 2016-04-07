fbdf0901f2396ad13eee8ca07e3387453922c572
bergquist <carl.bergquist@gmail.com>
2016-04-07 10:03:26 +0200

    adds some info print

--- build.sh
+++ build.sh
@@ -12,6 +12,7 @@ fi
 
 echo "GRAFANA_VERSION: ${_grafana_version}"
 echo "DOCKER TAG: ${_grafana_tag}"
+echo "RELEASE BUILD: ${_release_build}"
 
 docker build --build-arg GRAFANA_VERSION=${_grafana_version} --tag "grafana/grafana:${_grafana_tag}"  --no-cache=true .
 
