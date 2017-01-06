f307c2da5c6da5b29ada031d89cdd6e71abd38ac
Malian De Ron <malian.deron@euranova.eu>
2017-01-06 17:08:17 +0100

    Use GF_PATHS_PLUGINS variable to install plugin in the right directory.

--- run.sh
+++ run.sh
@@ -34,7 +34,7 @@ if [ ! -z "${GF_INSTALL_PLUGINS}" ]; then
   OLDIFS=$IFS
   IFS=','
   for plugin in ${GF_INSTALL_PLUGINS}; do
-    grafana-cli plugins install ${plugin}
+    grafana-cli  --pluginsDir "${GF_PATHS_PLUGINS}" plugins install ${plugin}
   done
   IFS=$OLDIFS
 fi
