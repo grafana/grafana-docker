c7e064093785c803b24453848eb9ed7f7b273466
Daniel Lee <dan.limerick@gmail.com>
2017-03-22 12:03:36 +0100

    Strips the v out of the version tag
    so that the download link to S3 is correct.

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 _grafana_version=$1
+_download_version=${_grafana_version:1:5}
 
 if [ "$_grafana_version" != "" ]; then
 	echo "Building version ${_grafana_version}"
 	docker build \
-		--build-arg DOWNLOAD_URL=https://s3-us-west-2.amazonaws.com/grafana-releases/release/grafana_${_grafana_version}_amd64.deb \
+		--build-arg DOWNLOAD_URL=https://s3-us-west-2.amazonaws.com/grafana-releases/release/grafana_${_download_version}_amd64.deb \
 		--tag "grafana/grafana:${_grafana_version}" \
 		--no-cache=true .
 	docker tag grafana/grafana:${_grafana_version} grafana/grafana:latest
