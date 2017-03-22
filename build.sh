03c04dfd5aec4efda11e866d04bd05473e813c2f
Daniel Lee <dan.limerick@gmail.com>
2017-03-22 12:15:19 +0100

    Strip the v out of the version for docker tags too

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-_grafana_version=$1
-_download_version=${_grafana_version:1:5}
+_grafana_tag=$1
+_grafana_version=${_grafana_tag:1:5}
 
 if [ "$_grafana_version" != "" ]; then
 	echo "Building version ${_grafana_version}"
-	echo "Download url: https://s3-us-west-2.amazonaws.com/grafana-releases/release/grafana_${_download_version}_amd64.deb"
+	echo "Download url: https://s3-us-west-2.amazonaws.com/grafana-releases/release/grafana_${_grafana_version}_amd64.deb"
 	docker build \
 		--build-arg DOWNLOAD_URL=https://s3-us-west-2.amazonaws.com/grafana-releases/release/grafana_${_download_version}_amd64.deb \
 		--tag "grafana/grafana:${_grafana_version}" \
