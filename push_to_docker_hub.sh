19496f4f7f6754d2a482d7ef0e21b30c6a4a3da9
Daniel Lee <dan.limerick@gmail.com>
2017-03-22 13:03:28 +0100

    Add back push latest tag

--- push_to_docker_hub.sh
+++ push_to_docker_hub.sh
@@ -6,7 +6,7 @@ _grafana_version=${_grafana_tag:1:5}
 if [ "$_grafana_version" != "" ]; then
 	echo "pushing grafana/grafana:${_grafana_version}"
 	docker push grafana/grafana:${_grafana_version}
-	#docker push grafana/grafana:latest
+	docker push grafana/grafana:latest
 else
 	echo "pushing grafana/grafana:master"
 	docker push grafana/grafana:master
