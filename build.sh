2e3b1b27370098a2226fe778a5db69218378a8fb
Daniel Lee <dan.limerick@gmail.com>
2017-03-22 12:44:31 +0100

    Fix for spelling mistake

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ if [ "$_grafana_version" != "" ]; then
 	echo "Building version ${_grafana_version}"
 	echo "Download url: https://s3-us-west-2.amazonaws.com/grafana-releases/release/grafana_${_grafana_version}_amd64.deb"
 	docker build \
-		--build-arg DOWNLOAD_URL=https://s3-us-west-2.amazonaws.com/grafana-releases/release/grafana_${_download_version}_amd64.deb \
+		--build-arg DOWNLOAD_URL=https://s3-us-west-2.amazonaws.com/grafana-releases/release/grafana_${_grafana_version}_amd64.deb \
 		--tag "grafana/grafana:${_grafana_version}" \
 		--no-cache=true .
 	docker tag grafana/grafana:${_grafana_version} grafana/grafana:latest
