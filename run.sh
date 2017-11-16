27f06d41fd9307f8d01845cf481bb7307614abf7
Karol Ruszczyk <karol.ruszczyk@stratslab.com>
2017-11-16 13:35:18 +0100

    gosu grafana when installing plugins

--- run.sh
+++ run.sh
@@ -35,7 +35,7 @@ if [ ! -z "${GF_INSTALL_PLUGINS}" ]; then
   IFS=','
   for plugin in ${GF_INSTALL_PLUGINS}; do
     IFS=$OLDIFS
-    grafana-cli  --pluginsDir "${GF_PATHS_PLUGINS}" plugins install ${plugin}
+    gosu grafana grafana-cli --pluginsDir "${GF_PATHS_PLUGINS}" plugins install ${plugin}
   done
 fi
 
