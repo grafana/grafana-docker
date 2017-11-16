199a2dae50feb32f409728e1f0e8c7a61fb5ea5b
Patrick Lucas <me@patricklucas.com>
2017-11-16 13:59:56 +0100

    Pass "$@" to grafana-server before 'cfg:' options
    Passing the provided args (`$@`) at the end of the command line
    invocation results in grafana-server ignoring them. This doesn't happen
    if they are passed before the 'cfg'' directives.
    
    To reproduce, run `docker run --rm grafana/grafana:4.6.2 -v`. This
    should print `Version 4.6.2 (commit: 8db5f08)` but instead starts
    Grafana.

--- run.sh
+++ run.sh
@@ -43,9 +43,9 @@ fi
 exec gosu grafana /usr/sbin/grafana-server              \
   --homepath=/usr/share/grafana                         \
   --config="$GF_PATHS_CONFIG"                           \
+  "$@"                                                  \
   cfg:default.log.mode="console"                        \
   cfg:default.paths.data="$GF_PATHS_DATA"               \
   cfg:default.paths.logs="$GF_PATHS_LOGS"               \
   cfg:default.paths.plugins="$GF_PATHS_PLUGINS"         \
-  cfg:default.paths.provisioning=$GF_PATHS_PROVISIONING \
-  "$@"
+  cfg:default.paths.provisioning=$GF_PATHS_PROVISIONING
