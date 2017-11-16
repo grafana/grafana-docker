4a4905f0096783fb755ce033960bc56d58ffe9d8
Karol Ruszczyk <karol.ruszczyk@stratslab.com>
2017-11-16 13:08:25 +0100

    make a target docker repo an optional param

--- build.sh
+++ build.sh
@@ -3,14 +3,16 @@
 _grafana_tag=$1
 _grafana_version=${_grafana_tag:1}
 
+_docker_repo=${2:-grafana/grafana}
+
 if [ "$_grafana_version" != "" ]; then
 	echo "Building version ${_grafana_version}"
 	echo "Download url: https://s3-us-west-2.amazonaws.com/grafana-releases/release/grafana_${_grafana_version}_amd64.deb"
 	docker build \
 		--build-arg DOWNLOAD_URL=https://s3-us-west-2.amazonaws.com/grafana-releases/release/grafana_${_grafana_version}_amd64.deb \
-		--tag "grafana/grafana:${_grafana_version}" \
+		--tag "${_docker_repo}:${_grafana_version}" \
 		--no-cache=true .
-	docker tag grafana/grafana:${_grafana_version} grafana/grafana:latest
+	docker tag ${_docker_repo}:${_grafana_version} ${_docker_repo}:latest
 
 else
 	echo "Building latest for master"
