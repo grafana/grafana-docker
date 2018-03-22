b42f62f98f91fa2600da6d05980e5742c344393b
Leonard Gram <leo@xlson.com>
2018-03-22 16:53:45 +0100

    Adds permission check when starting Grafana.

--- run.sh
+++ run.sh
@@ -7,6 +7,29 @@
 : "${GF_PATHS_PLUGINS:=/var/lib/grafana/plugins}"
 : "${GF_PATHS_PROVISIONING:=/etc/grafana/provisioning}"
 
+PERMISSIONS_OK=0
+
+if [ ! -r "$GF_PATHS_CONFIG" ]; then
+    echo "GF_PATHS_CONFIG='$GF_PATHS_CONFIG' is not readable."
+    PERMISSIONS_OK=1
+fi
+
+if [ ! -w "$GF_PATHS_DATA" ]; then
+    echo "GF_PATHS_DATA='$GF_PATHS_DATA' is not writable."
+    PERMISSIONS_OK=1
+fi
+
+if [ ! -r "$GF_PATHS_HOME" ]; then
+    echo "GF_PATHS_HOME='$GF_PATHS_HOME' is not readable."
+    PERMISSIONS_OK=1
+fi
+
+
+if [ $PERMISSIONS_OK -eq 1 ]; then
+    echo "You may have issues with file permissions, more information here: http://docs.grafana.org/installation/docker/"
+fi
+ 
+
 if [ ! -z ${GF_AWS_PROFILES+x} ]; then
     mkdir -p "$GF_PATHS_HOME/.aws/"
     > "$GF_PATHS_HOME/.aws/credentials"
